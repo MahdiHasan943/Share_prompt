0752d25d1fd9723a5ef3e70236269b9a68fd32e5
MahdiHasan943 <mahdifreelancer41@gmail.com>
2023-06-26 14:41:44 +0600

    complete navbar & next-auth signIn & signOut functon added done

---
+++ app/Create-prompt/page.jsx
@@ -0,0 +1,8 @@
+
+const CraatePrompt = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default CraatePrompt
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 import '@styles/globals.css'
 
 
@@ -9,19 +11,23 @@ const RootLayout  = ({children}) => {
   return (
     <html lang="en">
     <head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Document</title>
     </head>
           <body>
-          <div className='main'>
+        <Provider>
+        <div className='main'>
                   <div className='gradient' />
               </div>
               
-              <main className='app'>
+        <main className='app'>
+          <Nav />
+          
                   {children}
 
               </main>
+         </Provider>
         
     </body>
     </html>
